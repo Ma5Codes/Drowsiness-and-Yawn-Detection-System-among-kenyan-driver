0e6cb24e85bb119146cb7a4a01e68fecc9a9ea5e
Ma5Codes <117619833+Ma5Codes@users.noreply.github.com>
2025-04-01 10:27:54 +0300

    Fix dlib using dlib-bin

--- build.sh
+++ build.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-# Install required dependencies
-apt-get update && apt-get install -y \
-    cmake build-essential \
+# Update package list
+apt-get update
+
+# Install dependencies for dlib
+apt-get install -y cmake build-essential \
     libopenblas-dev liblapack-dev libx11-dev \
-    libgtk-3-dev python3-dev
+    libgtk-3-dev python3-dev python3-pip
 
 # Upgrade pip
 pip install --upgrade pip 
 
 # Install Python dependencies
 pip install --no-cache-dir -r requirements.txt
+pip install dlib-bin
+
